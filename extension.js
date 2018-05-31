f234e7c074bd3da1a604bb523ec93f7a76660e41
Stephen <wizebin@gmail.com>
2018-05-31 12:56:01 -0400

    Added Replace Eval which replaces selection with it's js evaluation

--- extension.js
+++ extension.js
@@ -6,23 +6,38 @@ const selectionReplaceFunctionCreator = (editorCallback) => {
         const selections = editor.selections;
         return editor.edit(editBuilder => {
             for (let dex = 0; dex < selections.length; dex += 1) {
-                editorCallback(editBuilder, selections[dex], dex);
+                editorCallback(editBuilder, editor, selections[dex], dex);
             }
         });
     }
 }
 
-const replaceCursorIndex = selectionReplaceFunctionCreator((editBuilder, selection, dex) => {
+const replaceCursorIndex = selectionReplaceFunctionCreator((editBuilder, editor, selection, dex) => {
     editBuilder.replace(selection, '' + dex);
 });
 
-const replaceLine = selectionReplaceFunctionCreator((editBuilder, selection, dex) => {
+const replaceLine = selectionReplaceFunctionCreator((editBuilder, editor, selection, dex) => {
     editBuilder.replace(selection, '' + (selection.start.line + 1));
+});
+
+const replaceEval = selectionReplaceFunctionCreator((editBuilder, editor, selection, dex) => {
+    const currentText = editor.document.getText(selection);
+    try {
+        const result = eval(currentText);
+        if (typeof result === 'object') {
+            editBuilder.replace(selection, JSON.stringify(result));
+        } else {
+            editBuilder.replace(selection, '' + result);
+        }
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 function activate(context) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.replace_line', replaceLine));
     context.subscriptions.push(vscode.commands.registerCommand('extension.replace_cursor_index', replaceCursorIndex));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.replace_eval', replaceEval));
 }
 
 function deactivate() {}
@@ -31,3 +46,4 @@ exports.activate = activate;
 exports.deactivate = deactivate;
 exports.replaceCursorIndex = replaceCursorIndex;
 exports.replaceLine = replaceLine;
+exports.replaceEval = replaceEval;
--- test/extension.test.js
+++ test/extension.test.js
@@ -3,6 +3,20 @@ const vscode = require('vscode');
 const extension = require('../extension');
 const replaceCursorIndex = extension.replaceCursorIndex;
 const replaceLine = extension.replaceLine;
+const replaceEval = extension.replaceEval;
+
+function clearCurrentDocument() {
+    return vscode.window.activeTextEditor.edit(editBuilder => {
+        const fullText = vscode.window.activeTextEditor.document.getText();
+
+        const fullRange = new vscode.Range(
+            vscode.window.activeTextEditor.document.positionAt(0),
+            vscode.window.activeTextEditor.document.positionAt(fullText.length - 1)
+        )
+
+        editBuilder.delete(fullRange);
+    });
+}
 
 /**
  * This is ugly AF, but unfortunately nesting these statements in a .then() chain fails. This
@@ -87,6 +101,12 @@ suite("Selection Replacement Tests", () => {
                     waiting=false;
                 });
             } else if (step === 2) {
+                waiting = true;
+                clearCurrentDocument().then(() => {
+                    step += 1;
+                    waiting = false;
+                });
+            } else if (step === 3) {
                 clearInterval(timeout);
 
                 globalEditor.edit(editBuilder => {
@@ -100,6 +120,60 @@ suite("Selection Replacement Tests", () => {
                     assert.equal(globalDocument.getText(), 'first\nsecond\nthird\nfourth\nfifth\nsixth');
                     replaceCursorIndex().then(wasReplaced => {
                         assert.equal(globalDocument.getText(), '0first\nsecond\nthird\nfourth\n1fifth\nsixth');
+
+                        done();
+                    }, (error) => { assert.fail(error);done(); });
+                }, (error) => { assert.fail(error);done(); });
+            }
+        }, 10);
+	});
+
+    test("Replaces Selection With Evaluated JS", (done) => {
+        let step = 0;
+        let globalDocument = null;
+        let globalEditor = null;
+        let timeout = null;
+        let waiting = false;
+
+
+        timeout = setInterval(() => {
+            if (waiting) return null;
+
+            if (step === 0) {
+                waiting = true;
+                vscode.workspace.openTextDocument().then((document) => {
+                    step += 1;
+                    globalDocument = document;
+                    waiting = false;
+                }, (error) => { assert.fail(error);done(); });
+            } else if (step === 1) {
+                waiting = true;
+                vscode.window.showTextDocument(globalDocument).then(editor => {
+                    step += 1;
+                    globalEditor = editor;
+                    waiting=false;
+                });
+            } else if (step === 2) {
+                waiting = true;
+                clearCurrentDocument().then(() => {
+                    step += 1;
+                    waiting = false;
+                });
+            } else if (step === 3) {
+                clearInterval(timeout);
+
+                globalEditor.edit(editBuilder => {
+                    editBuilder.insert(new vscode.Position(0, 0), '1+2+3\n2*2\n"a".repeat(3)');
+                }).then(wasEdited => {
+                    const innerEditor = vscode.window.activeTextEditor;
+                    innerEditor.selections = [
+                        new vscode.Selection(0, 0, 0, 5),
+                        new vscode.Selection(1, 0, 1, 3),
+                        new vscode.Selection(2, 0, 2, 13),
+                    ];
+                    assert.equal(globalDocument.getText(), '1+2+3\n2*2\n"a".repeat(3)');
+                    replaceEval().then(wasReplaced => {
+                        assert.equal(globalDocument.getText(), '6\n4\naaa');
 
                         done();
                     }, (error) => { assert.fail(error);done(); });
